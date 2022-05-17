fcbcdaa2b291ad58eef6b69c5c17f6b7ba60b21b
Sara Isa <saraisamo123@gmail>
2022-05-17 09:59:39 +0300

    image update

--- client/src/Components/products.js
+++ client/src/Components/products.js
@@ -15,19 +15,19 @@ function Products() {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 py-10 gri"> 
       <Link to="/buildingMaterials">
         <div className="h-72 my-6 bg-white shadow-xl rounded-md w-11/12 m-auto py-10">
-          <img src='./carrier.png' alt='carrier' className="h-2/3 w-2/3 items-center m-auto"/>
+          <img src='./qori cas 5m.JPG' alt='carrier' className="h-2/3 w-2/3 items-center m-auto"/>
           <h3 className="text-white pt-3 font-extrabold text-center h-20 w-full bg-red-red2">OUR <br />BUILDING MATERIALS</h3>
           </div>
           </Link>
           <Link to="/electricity">
         <div className=" h-72 my-6 bg-white shadow-xl rounded-md w-11/12 m-auto py-10">
-          <img src='./TV sockets .jpeg' alt='wood-oli' className="h-2/3 w-2/3 items-center m-auto" />
+          <img src='./wire.jpg' alt='wood-oli' className="h-2/3 w-2/3 items-center m-auto" />
           <h3 className="text-white pt-3 font-extrabold text-center h-20 w-full bg-red-red2">OUR <br />ELECTRICTY</h3>
           </div>
           </Link>
           <Link to="/plumbing">
         <div className=" h-72 my-6 bg-white shadow-xl rounded-md w-11/12 m-auto py-10">
-          <img src='./water measure.png' alt='carrier'className="h-2/3 w-2/3 items-center m-auto" />
+          <img src='./UPVC pipe .jpeg' alt='carrier'className="h-2/3 w-2/3 items-center m-auto" />
           <h3 className="text-white pt-3 font-extrabold text-center h-20 w-full bg-red-red2">OUR <br />PLUMBING</h3>
           </div>
           </Link>
